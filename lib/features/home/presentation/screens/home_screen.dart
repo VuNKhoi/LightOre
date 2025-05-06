44620741e207e608918084ecb6898bfc166dd8bd
VuNKhoi <35670841+VuNKhoi@users.noreply.github.com>
2025-05-06 04:58:31 -0700

    add log out button, update go router version

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lightore/auth/application/auth_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
       body: Center(
-        child: Text('Welcome to LightOre Home'),
+        child: ElevatedButton(
+          onPressed: () {
+            ref.read(isLoggedInProvider.notifier).state = false;
+          },
+          child: const Text('Log Out'),
+        ),
       ),
     );
   }
